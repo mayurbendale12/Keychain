d7936c02172e1075603ce1b060039d23fdfee672
Mayur Bendale <msbendale91@gmail.com>
2023-12-09 12:53:56 +0530

    Keychain implementation details

---
+++ Keychain/KeychainManager.swift
@@ -0,0 +1,102 @@
+//
+//  KeychainManager.swift
+//  Keychain
+//
+//  Created by Mayur Bendale on 07/12/23.
+//
+
+import Foundation
+
+class KeychainManager {
+    enum KeychainError: Error {
+        case duplicateEntry
+        case readError
+        case noPassword
+        case unknown(OSStatus)
+    }
+
+    static func save(service: String, username: String, password: String) throws {
+        let passwordData = password.data(using: .utf8)
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: username,
+            kSecValueData as String: passwordData as Any,
+        ]
+
+        let status = SecItemAdd(query as CFDictionary, nil)
+
+        guard status != errSecDuplicateItem else {
+            throw KeychainError.duplicateEntry
+        }
+
+        guard status == errSecSuccess else {
+            throw KeychainError.unknown(status)
+        }
+    }
+
+    static func get(service: String, username: String) throws -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: username,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary,
+                                         &result)
+        guard status == errSecSuccess else {
+            throw KeychainError.unknown(status)
+        }
+
+        guard let data = result as? Data else {
+            throw KeychainError.readError
+        }
+
+        return String(data: data, encoding: .utf8)
+    }
+
+    static func update(service: String, username: String, password: String) throws {
+        let passwordData = password.data(using: .utf8)
+
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service
+        ]
+
+        let attributes: [String: Any] = [
+            kSecAttrAccount as String: username,
+            kSecValueData as String: passwordData as Any
+        ]
+
+        let status = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
+        guard status != errSecItemNotFound else { throw KeychainError.noPassword }
+        guard status == errSecSuccess else { throw KeychainError.unknown(status) }
+    }
+
+    static func delete(service: String) throws {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service
+        ]
+
+        let status = SecItemDelete(query as CFDictionary)
+        guard status == errSecSuccess || status == errSecItemNotFound else { throw KeychainError.unknown(status) }
+    }
+
+    static func clearKeychain() {
+        let secClasses: [AnyObject] = [kSecClassGenericPassword,
+                                      kSecClassInternetPassword,
+                                      kSecClassCertificate,
+                                      kSecClassKey,
+                                      kSecClassIdentity]
+        secClasses.forEach { secClass in
+            let query: [String: Any] = [
+                kSecClass as String: secClass
+            ]
+            let _ = SecItemDelete(query as CFDictionary)
+        }
+    }
+}
---
+++ Keychain/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  Keychain
+//
+//  Created by Mayur Bendale on 07/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var updatePasswordTextField: UITextField!
+    @IBOutlet private weak var getPasswordLabel: UILabel!
+    
+    private let service = "abc.com"
+
+    private func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alertController, animated: true)
+    }
+
+    @IBAction private func didTapSaveButton(_ sender: UIButton) {
+        guard let username = usernameTextField.text, !username.isEmpty else {
+            showAlert(title: "Failed", message: "Enter UserName")
+            return
+        }
+
+        guard let password = passwordTextField.text, !password.isEmpty else {
+            showAlert(title: "Failed", message: "Enter Password")
+            return
+        }
+
+        do {
+            try KeychainManager.save(service: service,
+                                 username: usernameTextField.text ?? "",
+                                 password: passwordTextField.text ?? "")
+        } catch {
+            print(error)
+        }
+
+        usernameTextField.text = ""
+        passwordTextField.text = ""
+    }
+
+    @IBAction private func didTapGetButton(_ sender: UIButton) {
+        guard let username = usernameTextField.text, !username.isEmpty else {
+            showAlert(title: "Failed", message: "Enter UserName")
+            return
+        }
+        do {
+            let password = try KeychainManager.get(service: service, username: usernameTextField.text ?? "")
+            getPasswordLabel.text = password
+        } catch {
+            getPasswordLabel.text = ""
+            showAlert(title: "Failed", message: "No Password exist")
+            print(error)
+        }
+    }
+
+    @IBAction private func didTapUpdateButton(_ sender: UIButton) {
+        guard let username = usernameTextField.text, !username.isEmpty else {
+            showAlert(title: "Failed", message: "Enter UserName")
+            return
+        }
+        do {
+            try KeychainManager.update(service: service,
+                                       username: usernameTextField.text ?? "",
+                                       password: updatePasswordTextField.text ?? "")
+        } catch {
+            print(error)
+        }
+        updatePasswordTextField.text = ""
+    }
+
+    @IBAction private func didTapDeleteButton(_ sender: UIButton) {
+        do {
+            try KeychainManager.delete(service: service)
+        } catch {
+            print(error)
+        }
+    }
+
+    @IBAction private func didTapClearKeychainButton(_ sender: UIButton) {
+        KeychainManager.clearKeychain()
+        usernameTextField.text = ""
+        passwordTextField.text = ""
+        updatePasswordTextField.text = ""
+        getPasswordLabel.text = ""
+    }
+}
